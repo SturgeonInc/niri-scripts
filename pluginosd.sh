cf4d2f1a7341ba17b185bfcdf59dbe594b6ebd26
Robi Jenik <robijenik@gmail.com>
2025-08-26 15:58:07 -0400

    pluginosd.sh SOUND_EFFECT_FOLDER included in SOUND_EFFECT_* path rather than in play command

--- pluginosd.sh
+++ pluginosd.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 
 SOUND_EFFECT_FOLDER="$HOME/Music/sound-effects"
-SOUND_EFFECT_DISCHARGE="button_synth_negative_02.wav"
-SOUND_EFFECT_CHARGE="sound_buttons_button_synth_positive_01.wav"
+SOUND_EFFECT_DISCHARGE="$SOUND_EFFECT_FOLDER/button_synth_negative_02.wav"
+SOUND_EFFECT_CHARGE="$SOUND_EFFECT_FOLDER/sound_buttons_button_synth_positive_01.wav"
 
 VOLUME=0.5
 
@@ -11,14 +11,14 @@ upower --monitor-detail | while read -r LINE; do
   state:*discharging)
     if [ "$STATE" != DISCHARGING ]; then
       swayosd-client --custom-message="Battery Discharging" --custom-icon='battery-good-symbolic'
-      play --volume "$VOLUME" "$SOUND_EFFECT_FOLDER/$SOUND_EFFECT_DISCHARGE"
+      play --volume "$VOLUME" "$SOUND_EFFECT_DISCHARGE"
       STATE=DISCHARGING
     fi
     ;;
   state:*pending-charge | state:*charging)
     if [ "$STATE" != CHARGING ]; then
       swayosd-client --custom-message="Battery Charging" --custom-icon='battery-full-charged-symbolic'
-      play --volume "$VOLUME" "$SOUND_EFFECT_FOLDER/$SOUND_EFFECT_CHARGE"
+      play --volume "$VOLUME" "$SOUND_EFFECT_CHARGE"
       STATE=CHARGING
     fi
     ;;
