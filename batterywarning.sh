d5452a46d5da17a9becd9062ea2e149fc199ec68
Robi <robijenik@gmail.com>
2025-11-08 13:44:22 -0500

    battery warning style changes

--- batterywarning.sh
+++ batterywarning.sh
@@ -1,41 +1,43 @@
 #!/usr/bin/env sh
 
-INTERVAL='1m'
-BATTERY='/org/freedesktop/UPower/devices/battery_BAT1'
-POWER_SUPPLY_STATUS='/sys/class/power_supply/BAT1/status'
+readonly INTERVAL='1m'
+readonly BATTERY='/org/freedesktop/UPower/devices/battery_BAT1'
+readonly POWER_SUPPLY_STATUS='/sys/class/power_supply/BAT1/status'
+
+readonly CRIT_ALERT_SOUND="$HOME/Music/sound-effects/sound_ambient_alarms_portal_elevator_chime.wav"
 
 # Thresholds (highest to lowest)
 set -- 15 10 5
 
 MINIMUM_THRESH="$1"
-STATE=q0
+state=q0
 
 while true; do
-  PERC="$(upower -i "$BATTERY" | grep percentage)"
-  PERC=${PERC##* } # get last word (percentage of battery)
-  PERC=${PERC%\%}  # remove percentage sign
+  perc="$(upower -i "$BATTERY" | grep percentage)"
+  perc=${perc##* } # get last word (percentage of battery)
+  perc=${perc%\%}  # remove percentage sign
 
-  STATE_PREV="$STATE"
+  STATE_PREV="$state"
   >&2 echo "STATE_PREV=$STATE_PREV"
 
   # state handling
-  if [ "$PERC" -gt "$1" ]; then
-    STATE="above${MINIMUM_THRESH}"
+  if [ "$perc" -gt "$1" ]; then
+    state="above${MINIMUM_THRESH}"
   else # STATE's final value is the last (and therefore loweset) threshold passed
     for thresh in "$@"; do
-      if [ "$PERC" -le "$thresh" ]; then
-        STATE="sub${thresh}"
+      if [ "$perc" -le "$thresh" ]; then
+        state="sub${thresh}"
       fi
     done
   fi
-  >&2 echo "state is now $STATE"
+  >&2 echo "state is now $state"
 
   if [ "$(cat "$POWER_SUPPLY_STATUS")" != Charging ]; then
     >&2 echo 'Not charging'
-    if [ "$STATE_PREV" != "$STATE" ] && [ "$PERC" -le "$MINIMUM_THRESH" ]; then
+    if [ "$STATE_PREV" != "$state" ] && [ "$perc" -le "$MINIMUM_THRESH" ]; then
       >&2 echo "States different and perc <= $THRESH3; playing notification"
-      notify-send --urgency=critical --icon "battery-empty" "LOW BATTERY WARNING: $PERC%"
-      play /home/fisherman/Music/sound-effects/sound_ambient_alarms_portal_elevator_chime.wav
+      notify-send --urgency=critical --icon "battery-empty" "LOW BATTERY WARNING: $perc%"
+      play "$CRIT_ALERT_SOUND"
     fi # current vs prev state
   fi   # chargin
 
